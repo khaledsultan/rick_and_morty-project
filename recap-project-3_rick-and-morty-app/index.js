3867e132986f994b258f9eb28e28d70c6a476baf
Khaled sultan <eng-sultan.k@hotmail.com>
2023-06-19 14:51:11 +0200

    project_3

--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -1,3 +1,5 @@
+// import {createCharacterCard} from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,29 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    let response = await fetch("https://swapi.dev/api/people");
+
+    if (response.ok) {
+      // Success (Good Response)
+      let data = await response.json();
+      let people = data.results;
+      // console.log(data);
+      // console.log({ results } );
+
+      console.log(people);
+
+      people.forEach((element) => {
+        renderElement(Card(element));
+      });
+    } else {
+      // Failure (Bad Response)
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    // Failure (Network error, etc)
+    console.error("An Error occurred");
+  }
+}
